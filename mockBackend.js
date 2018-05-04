5b079a42f69cf14c652a4d485610215b6468fb20
Jipos <nonoma@hotmail.com>
2018-05-04 19:46:43 +0200

    Remove mockBackend code
    Because the mockjax approach seems to work rather well, the mockBackend code is no longer needed.

--- mockBackend.js
+++
@@ -1,6 +0,0 @@
-// Export the MockBackend middleware
-module.exports = function(app) {
-  app.get('/users/me', function (req, res) {
-    return res.json({id: 123, userid: 'u0076006', firstName: 'Kevin', lastName: 'Rogiers'});
-  });
-}
