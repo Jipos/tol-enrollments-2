de04c28ee3dac3efecdbce876b7dbad13e3e5667
Jipos <nonoma@hotmail.com>
2018-05-07 21:22:49 +0200

    Add content to header component (attempt 1)

--- src/components/header/controller.js
+++ src/components/header/controller.js
@@ -14,7 +14,16 @@ const HeaderController = ApplicationController.extend({
 
   getHeaderView: function() {
     // TODO: KR get model from somewhere
-    const collection = new Bb.Collection([{id: 1, name: 'enroll'}, {id: 2, name: 'listEnrollments'}]);
+    const collection = new Bb.Collection([
+      {id: 1, name: 'enroll', options: [
+        {id: 11, name: 'foo-11'},
+        {id: 12, name: 'foo-12'}
+      ]},
+      {id: 2, name: 'listEnrollments', options: [
+        {id: 21, name: 'foo-21'},
+        {id: 22, name: 'foo-22'}
+      ]},
+    ]);
     return new HeaderView({collection: collection});
   }
 
--- src/components/header/views.js
+++ src/components/header/views.js
@@ -1,31 +1,66 @@
+import Bb from 'backbone';
 import Mn from 'backbone.marionette';
 import headerTemplate from './header.hbs';
 
-const HeaderLinkView = Mn.View.extend({
-  constructorName: 'HeaderLinkView',
-  tagName: 'span',
+const OptionView = Mn.View.extend({
+  constructorName: 'OptionView',
+  tagName: 'option',
+  attributes: {name: 'optionview'},
   template: function (data) {
-    return ` | ${data.name}`;
+    return `<option>${data.name}</option>`;
+  },
+  onRender: function () {
+    console.log('option rendered');
+    // Remove wrapper element. This assumes the template contains only one root element.
+    // this.$el = this.$el.children();
   }
 })
 
-export const HeaderLinksView = Mn.NextCollectionView.extend({
-  constructorName: 'HeaderLinksView',
-  childView: HeaderLinkView
+const OptionsView = Mn.NextCollectionView.extend({
+  constructorName: 'OptionsView',
+  attributes: {name: 'optionsview'},
+  childView: OptionView,
+  tagName: 'optgroup',
+});
+
+const OptgroupView = Mn.View.extend({
+  constructorName: 'OptgroupView',
+  tagName: 'optgroup',
+  attributes: {name: 'optgroupview'},
+  template: () => '<optgroup></optgroup>',
+  regions: {
+    optgroup: {
+      el: 'optgroup',
+      replaceElement: true
+    }
+  },
+  onRender: function () {
+    this.showChildView('optgroup', new OptionsView({
+      collection: new Bb.Collection(this.model.get('options'))
+    }));
+  }
+})
+
+const OptgroupsView = Mn.NextCollectionView.extend({
+  constructorName: 'OptgroupsView',
+  childView: OptgroupView,
+  attributes: {name: 'optgroupsview'},
+  tagName: 'select'
 });
 
 export const HeaderView = Mn.View.extend({
   constructorName: 'HeaderView',
   template: headerTemplate,
+  attributes: {name: 'headerview'},
   regions: {
     links: {
       el: '.links',
-      replace: true
+      replaceElement: true
     }
   },
   onRender: function () {
-    this.showChildView('links', new HeaderLinksView({
-      collection: this.collection
+    this.showChildView('links', new OptgroupsView({
+      collection: this.collection,
     }));
   }
 });
